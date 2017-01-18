5b6c257d2bac93d12a63779c81c8d8143ccb3ebd
njarron <hao.zhang@baoz.cn>
2017-01-18 17:40:45 +0800

    version 1 completed

---
+++ src/components/ios/ios.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-list :type="'iOS'"></v-list>
+</template>
+
+<script>
+import vList from '../list/list.vue'
+export default {
+components: {
+    vList
+}
+}
+</script>
+<style lang="stylus">
+</style>
--- src/routers.js
+++ src/routers.js
@@ -1,6 +1,9 @@
 const App = resolve => require(['./App.vue'], resolve)
 const Welfare = resolve => require(['./components/welfare/welfare.vue'], resolve)
 const Recommand = resolve => require(['./components/recommand/recommand.vue'], resolve)
+const Android = resolve => require(['./components/android/android.vue'], resolve)
+const Ios = resolve => require(['./components/ios/ios.vue'], resolve)
+const Web = resolve => require(['./components/web/web.vue'], resolve)
 
 const routers = [{
     path: '/',
@@ -15,6 +18,18 @@ const routers = [{
         path: '/day',
         name: 'recommand',
         component: Recommand
+    }, {
+        path: '/android',
+        name: 'android',
+        component: Android
+    }, {
+        path: '/ios',
+        name: 'ios',
+        component: Ios
+    }, {
+        path: '/web',
+        name: 'web',
+        component: Web
     }]
 }]
 
